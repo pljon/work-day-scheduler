52f265b237cc128ccaeb151ee83cd3270281597f
Jonathan Plaras <jonplaras@gmail.com>
2022-12-05 14:14:29 -0500

    updated javascript so localstorage saves only on button click of corresponding hour time-block, not when focusing text area

--- Develop/script.js
+++ Develop/script.js
@@ -20,52 +20,61 @@ var todayHours = Array.from(new Array(24)).map((v, i) => {
 // else if time is less than today (see global variable)
 // if true, assign past
 // else if false (time is greater), assign future
+
+
 function color(time) {
+  // return <parameter>
   return time.text === currentTime.text 
+  // ternary operators
+  // <to do if false> ? <to do if true>
+  // 
   ? "present"
   : time.hour < today
   ? "past"
   : "future";
 }
 
+
 // For each hour in todayHours array, create grid with id in text format (ln. 10)
 // forEach creates new divs
-todayHours.forEach((hr) => {
-  var grid = $(
-    `<div id="${hr.text}" class="row time-block"> </div>`
-  );
-  // Each index.text will displays as the time for each timeblock
-  var time = $(
-    `<div class="col-2 col-md-1 hour text-center py-3">${hr.text}</div>`
-  );
-  // Name set to hr.text so inputted text can be referenced when 
-  // saving /  getting to / from localstorage
-  var textArea = $(
-    `<textarea name="${hr.text}" class="col-8 col-md-10 description ${color(hr)}" rows="3">${store.getItem(hr.text) || ""}</textarea>`
-  );
+$(function () {
+  todayHours.forEach((hr) => {
+    var grid = $(
+      `<form id="${hr.text}" class="row time-block"> </form>`
+    );
+    // Each index.text will displays as the time for each timeblock
+    var time = $(
+      `<div class="col-2 col-md-1 hour text-center py-3">${hr.text}</div>`
+    );
+    // Name set to hr.text so inputted text can be referenced when 
+    // saving /  getting to / from localstorage
+    var textArea = $(
+      `<textarea name="${hr.text}" class="col-8 col-md-10 description ${color(hr)}" rows="3">${store.getItem(hr.text) || ""}</textarea>`
+    );
 
-  var saveButton = $(
-    `<button class="btn saveBtn col-2 col-md-1" aria-label="save">
-    <i class="fas fa-save" aria-hidden="true"></i>
-    </button>`
-  );
-  // Click event listener to take value in textarea element to be saved in
-  // localstorage
-  grid.click((e) => {
-    e.preventDefault();
-    // value represents the text value in text area element
-    // saves to localstorage as hr.text which can be pulled from localstorage
-    // in line 44 text area element
-    var value = $(`textarea[name="${hr.text}"]`).val();
-    store.setItem(hr.text, value);
-  })
+    var saveButton = $(
+      `<button class="btn saveBtn col-2 col-md-1" aria-label="save">
+      <i class="fas fa-save" aria-hidden="true"></i>
+      </button>`
+    );
+    // Click event listener to take value in textarea element to be saved in
+    // localstorage
+    grid.submit((e) => {
+      e.preventDefault();
+      // value represents the text value in text area element
+      // saves to localstorage as hr.text which can be pulled from localstorage
+      // in line 44 text area element
+      var value = $(`textarea[name="${hr.text}"]`).val();
+      store.setItem(hr.text, value);
+    })
 
-  // Appends grid as child of container, appends rest as child under grid
-  container.append(grid);
-  grid.append(time);
-  grid.append(textArea);
-  grid.append(saveButton);
-  
+    // Appends grid as child of container, appends rest as child under grid
+    container.append(grid);
+    grid.append(time);
+    grid.append(textArea);
+    grid.append(saveButton);
+    
+  });
 });
 
 
@@ -73,7 +82,6 @@ todayHours.forEach((hr) => {
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -92,4 +100,3 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# work-day-scheduler
+# Work Scheduler
+
+## Description
+
+This is a 24 hour work day scheduler application that allows a user to insert a task in the form of text in the assigned time block for each hour according to the user's local date and time. The application displays hour time-blocks that have passed / ended as grey. The current hour time-block is displayed as red. The hour time-blocks in the future, are displayed as green. <br />
+<br />
+This application will save entered text through the button on the far-right column of the hour time-block's row for revisitation, if needed.
+
+## Installation
+
+Using git bash (Windows) or terminal (MacOS):
+
+1. Download ZIP found under `<> Code` and run the repo to open into VS Code <br />
+OR clone the repo using `git clone git@github.com:pljon/work-day-scheduler.git`
+2. Path to repo's location on local machine using `cd`
+3. Open repo on VS code using `code .` to begin working on it locally
+
+## Remote Link
+
+This is the deployed application that reflects the current source code found in this repo:
+
+https://pljon.github.io/work-day-scheduler/
+
+## Screenshot
+
+![website screenshot](./assets/images/quiz.gif)
